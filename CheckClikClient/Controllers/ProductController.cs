22475856ccbc47dfe48f0746a4e88e7f021b5831
checkclik-bit <114485990+checkclik-bit@users.noreply.github.com>
2023-05-13 15:29:34 +0500

    git ignore and fix conflict.

--- CheckClikClient/Controllers/ProductController.cs
+++ CheckClikClient/Controllers/ProductController.cs
@@ -67,6 +67,10 @@ namespace CheckClikClient.Controllers
              
             return View(lstBranchDTO);
         }
+        public async Task<ActionResult> CategoryDetails(int storeId, string q)
+        {
+            return View();
+        }
 
 
         [HttpPost]
