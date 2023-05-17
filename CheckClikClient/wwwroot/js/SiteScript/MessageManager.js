2885a8d4ed24f120a725230c84f3b09df0449d02
kamrultest24 <kamrultest24@gmail.com>
2023-05-17 20:15:53 +0600

    Remove Unnecessary lines from Login page, And added Toastr in Common Place, Also Fixed Toastr message in Common Manager.

---
+++ CheckClikClient/wwwroot/js/SiteScript/MessageManager.js
@@ -0,0 +1,22 @@
+﻿var MessageManager = { 
+    ShowMessage: function (type, message) {
+        toastr.options = {
+            "closeButton": true,
+            "debug": true,
+            "newestOnTop": false,
+            "progressBar": true,
+            "positionClass": "toast-top-right",
+            "preventDuplicates": true,
+            "onclick": null,
+            "showDuration": "300",
+            "hideDuration": "1000",
+            "timeOut": "5000",
+            "extendedTimeOut": "1000",
+            "showEasing": "swing",
+            "hideEasing": "linear",
+            "showMethod": "fadeIn",
+            "hideMethod": "fadeOut"
+        };
+        toastr[type](message);
+    }
+}
